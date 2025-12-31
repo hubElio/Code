dedbdcfb75c9301974816faf82cd1c326fc439df
hubEliot <eliot.ervais@gmail.com>
2025-12-31 14:11:49 -0500

    123125

---
+++ Projects/AI/DeepLearningTensorFlow/src/utils/TensorflowCNN.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# https://www.youtube.com/watch?v=Tq4K3LEMx4M&t=152s
+
+def main():
+    # Check TensorFlow version
+    print("TensorFlow Version:", tf.__version__)
+    
+    pass
+
+if __name__ == "__main__":
+    main()
